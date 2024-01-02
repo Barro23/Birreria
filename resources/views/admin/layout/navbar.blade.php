7f88d2017efeb7f2119f688ab8e024206f6a30d7
Barro23 <pb948697@gmail.com>
2024-01-02 14:39:05 -0300

    começando admin

---
+++ resources/views/admin/layout/navbar.blade.php
@@ -0,0 +1,53 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-lg text-light"  style="background-color:#1d1d1b;">
+        <div class="container">
+            <a class="navbar-brand" href="{{url('/admin')}}"><img src="{{asset('images/logo_ifg.png')}}" height="64" ></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body" style="background-color:#1d1d1b;">
+                    <ul class="navbar-nav d-flex justify-content-end flex-grow-1 pe-5">
+                        <li class="nav-item ">
+                            <a class="nav-link px-3 fw-bold" href="{{url('/admin')}}">INÍCIO</a>
+                        </li>
+
+
+                        @if ($_COOKIE['ADM_TIPO'] == 0)
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/proponente')}}">PROPONENTES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/eventos')}}">EVENTOS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/adicionar-usuario')}}">USUÁRIOS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/logs')}}">LOGS</a>
+                            </li>
+                            
+                        @elseif($_COOKIE['ADM_TIPO'] == 1)
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/adicionar-usuario')}}">USUÁRIOS</a>
+                            </li>
+                        @elseif($_COOKIE['ADM_TIPO'] == 2)
+                            <li class="nav-item">
+                                <a class="nav-link px-3 fw-bold" href="{{url('/admin/presenca/eventos')}}">EVENTOS</a>
+                            </li>
+                        @endif
+
+
+                        <li class="nav-item">
+                            <a class="nav-link px-3 fw-bold  " href="{{url('/admin/sair')}}">LOGOUT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/admin/template.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{asset('images\logo.png')}}">
+    <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=1.0">
+    <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}?v=1.0">
+    <link rel="stylesheet" href="{{asset("css\app.css")}}?v=1.0">
+    <title>SECITEC</title>
+</head>
+
+    
+<body style="background-color: rgba(150, 150, 150, 0.192);">
+
+    <header>
+        @include('admin.layout.navbar')
+    </header>
+
+    <div id="alert"></div>
+
+    <section class="section-padding">
+        @yield('content')
+    </section>
+
+    <footer class="footer mt-auto py-3">
+        @include('admin.layout.footer')
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/desvg@1.0.2/desvg.min.js"></script>
+    <script>
+        var alerta = document.getElementById("alert");
+
+        if(localStorage.getItem("message") != null && localStorage.getItem("type") != null){
+            alerta.innerHTML =
+            '<div class="z-3 position-fixed top-0 end-0" style="margin-top: 100px;">' +
+            '    <div class="alert alert-'+localStorage.getItem("type")+' alert-dismissible" role="alert">'+
+            '        <div>'+localStorage.getItem("message")+'</div>'+
+            '        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'+
+            '    </div>'+
+            '</div>';
+            localStorage.clear();
+        }
+
+        function showAlert(message, type) {
+            alerta.innerHTML =
+            '<div class="z-3 position-fixed top-0 end-0" style="margin-top: 100px;">' +
+            '    <div class="alert alert-'+type+' alert-dismissible" role="alert">'+
+            '        <div>'+message+'</div>'+
+            '        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'+
+            '    </div>'+
+            '</div>';
+        }
+
+    </script>
+</body>
+
+</html>
