b5a8e59f57a03c0705aae12531a222eb32869b80
Barro23 <pb948697@gmail.com>
2024-01-06 22:10:22 -0300

    template

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Birreria">
-    <link rel="icon" href="{{asset('images\logo.png')}}">
+    <link rel="icon" href="{{asset('')}}">
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap.css")}}?v=1.0">
     <link rel="stylesheet" href="{{asset("css\bootstrap\bootstrap-icons.css")}}?v=1.0">
     <link rel="stylesheet" href="{{asset('css\templatemo-leadership-event.css')}}?v=1.0">
